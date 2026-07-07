2180b7f8a4c96ca37e5028a565abcf5bd19c2226
Rodrigo Martinez <smartcripto2000@gmail.com>
2026-07-07 15:57:18 -0300

    Avalia candidatos de autotreinamento paper em quarentena

---
+++ smartcrypto/learning/paper_autotrain_quarantine_candidate_evaluation/__init__.py
@@ -0,0 +1,5 @@
+"""Paper auto-training quarantine candidate evaluation package."""
+
+from .evaluation import build_paper_autotrain_quarantine_candidate_evaluation_v1
+
+__all__ = ["build_paper_autotrain_quarantine_candidate_evaluation_v1"]
