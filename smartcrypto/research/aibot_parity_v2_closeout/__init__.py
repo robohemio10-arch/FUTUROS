fe86e00e951cab526df02628ffe7484fd6320f9a
robohemio10-arch <robohemio10@gmail.com>
2026-09-03 15:37:27 -0300

    Exporta auditor final do AIBOT Parity V2

---
+++ smartcrypto/research/aibot_parity_v2_closeout/__init__.py
@@ -0,0 +1,5 @@
+"""AIBOT Parity V2 final software DoD closeout audit."""
+
+from .auditor import SCHEMA_VERSION, WAVE_EVIDENCE, audit_aibot_parity_v2
+
+__all__ = ["SCHEMA_VERSION", "WAVE_EVIDENCE", "audit_aibot_parity_v2"]
