546248460f87e563bbcdce31f476b4f7d903d100
robohemio10-arch <robohemio10@gmail.com>
2026-09-05 17:52:12 -0300

    Exporta validação econômica OOS Paper

--- smartcrypto/learning/paper_autolearning/__init__.py
+++ smartcrypto/learning/paper_autolearning/__init__.py
@@ -5,6 +5,7 @@ from .daily_foundation_runner import build_paper_autolearning_foundation_report
 from .economic_challenger_scorecard import (
     build_paper_autolearning_economic_challenger_scorecard_v1,
 )
+from .economic_oos_validation import build_paper_autolearning_economic_oos_validation_v1
 from .live_feedback_loop import run_paper_autolearning_live_feedback_loop_v1
 from .outcome_schema import OUTCOME_EVENT_COLUMNS, SAFETY_FLAGS, SCHEMA_VERSION
 from .runtime_source import load_authoritative_closed_paper_trades
@@ -15,6 +16,7 @@ __all__ = [
     "SAFETY_FLAGS",
     "SCHEMA_VERSION",
     "build_paper_autolearning_economic_challenger_scorecard_v1",
+    "build_paper_autolearning_economic_oos_validation_v1",
     "build_paper_autolearning_foundation_report",
     "build_paper_autolearning_scheduler_report",
     "load_authoritative_closed_paper_trades",
