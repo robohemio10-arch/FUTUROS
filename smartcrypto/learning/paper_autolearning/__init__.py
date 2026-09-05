848a6e3f9110722df0ced33ac0dc1b7cf0a026be
robohemio10-arch <robohemio10@gmail.com>
2026-09-05 09:46:32 -0300

    Exporta loop Paper incremental

--- smartcrypto/learning/paper_autolearning/__init__.py
+++ smartcrypto/learning/paper_autolearning/__init__.py
@@ -1,7 +1,9 @@
 """Paper/shadow auto-learning foundation loop."""
 
 from .daily_foundation_runner import build_paper_autolearning_foundation_report
+from .live_feedback_loop import run_paper_autolearning_live_feedback_loop_v1
 from .outcome_schema import OUTCOME_EVENT_COLUMNS, SAFETY_FLAGS, SCHEMA_VERSION
+from .runtime_source import load_authoritative_closed_paper_trades
 from .scheduler import build_paper_autolearning_scheduler_report
 
 __all__ = [
@@ -10,4 +12,6 @@ __all__ = [
     "SCHEMA_VERSION",
     "build_paper_autolearning_foundation_report",
     "build_paper_autolearning_scheduler_report",
+    "load_authoritative_closed_paper_trades",
+    "run_paper_autolearning_live_feedback_loop_v1",
 ]
