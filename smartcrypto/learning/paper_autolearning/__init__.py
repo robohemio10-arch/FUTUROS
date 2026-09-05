04509a306601fd3952d2481efba32f45f794289f
robohemio10-arch <robohemio10@gmail.com>
2026-09-05 17:32:29 -0300

    Exporta scorecard econômico Paper

--- smartcrypto/learning/paper_autolearning/__init__.py
+++ smartcrypto/learning/paper_autolearning/__init__.py
@@ -2,6 +2,9 @@
 
 from .continuous_orchestrator import run_paper_autolearning_continuous_orchestrator_v1
 from .daily_foundation_runner import build_paper_autolearning_foundation_report
+from .economic_challenger_scorecard import (
+    build_paper_autolearning_economic_challenger_scorecard_v1,
+)
 from .live_feedback_loop import run_paper_autolearning_live_feedback_loop_v1
 from .outcome_schema import OUTCOME_EVENT_COLUMNS, SAFETY_FLAGS, SCHEMA_VERSION
 from .runtime_source import load_authoritative_closed_paper_trades
@@ -11,6 +14,7 @@ __all__ = [
     "OUTCOME_EVENT_COLUMNS",
     "SAFETY_FLAGS",
     "SCHEMA_VERSION",
+    "build_paper_autolearning_economic_challenger_scorecard_v1",
     "build_paper_autolearning_foundation_report",
     "build_paper_autolearning_scheduler_report",
     "load_authoritative_closed_paper_trades",
